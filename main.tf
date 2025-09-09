1c7252ca98483463020aa51f2fb627dfaa630547
naninanides <baihaqi.nugraha@salt.co.id>
2025-09-10 01:29:44 +0700

    add gitignore

--- main.tf
+++ main.tf
@@ -3,13 +3,22 @@ locals {
     var.domain_name
   )
 }
+resource "cloudflare_dns_record" "record_A" {
+  zone_id = var.zone_id
+  name = "@"
+  ttl = 1
+  type = "A"
+  content = "34.50.74.26"
+  proxied = true
+}
+
 resource "cloudflare_dns_record" "test" {
   zone_id = var.zone_id
   for_each = local.domain_name_list
   name = each.key
   ttl = 1
   type = "CNAME"
-  comment = "testing record dns"
-  content = var.domain
+  comment = "add domain using terraform"
+  content = each.value
   proxied = true
 }
--- variable.tf
+++ variable.tf
@@ -8,11 +8,6 @@ variable "zone_id" {
   type        = string
 }
 
-variable "domain" {
-  description = "core domain"
-  type        = string
-}
-
 variable "email_cloudflare" {
   description = "email cloudflare i using"
   type        = string
